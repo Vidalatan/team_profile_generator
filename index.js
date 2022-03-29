e2f9142ecef1a4b46751710c276ba9a81e7bf4c6
Vidalatan <vidalatan@gmail.com>
2022-03-29 13:47:45 -0500

    Updated index.js
    Added menuing section that will ask the user what kind of team member
    they wish to add.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const gather = require('./src/gather');
 const generate = require('./src/generateHTML');
+const inquirer = require('inquirer')
 
 async function init() {
     const manager = await gather.gatherManager();
-    const engineers = await gather.gatherEngineers();
-    const interns = await gather.gatherInterns();
-    generate.generateCards([manager, ...engineers, ...interns])
-    generate.writeHTML([manager, ...engineers, ...interns])
+    let engineers = []
+    let interns = []
+    
+    while (true) {
+        let response = await inquirer.prompt({
+            type: 'list',
+            name: 'addWhat',
+            message: 'Please select one of the following options',
+            choices: 
+            [
+                'Add Engineers',
+                'Add Interns',
+                'Done adding members'
+            ]
+        })
+
+        if (response.addWhat === 'Add Engineers') {
+            engineers = engineers.concat(await gather.gatherEngineers())
+        } else if (response.addWhat === 'Add Interns') {
+            interns = interns.concat(await gather.gatherInterns())
+        } else if (response.addWhat === 'Done adding members') {
+            generate.generateCards([manager, ...engineers, ...interns])
+            generate.writeHTML([manager, ...engineers, ...interns])
+            break;
+        }
+    }
 }
 init();
